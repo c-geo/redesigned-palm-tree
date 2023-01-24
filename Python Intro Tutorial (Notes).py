344e9ce72184f794c075d79eeb1da6e36ec3abee
Caroline George <120533177+c-geo@users.noreply.github.com>
2023-01-24 14:22:56 -0500

    Add files via upload

---
+++ Python Intro Tutorial (Notes).py
@@ -0,0 +1,53 @@
+# Python for beginners tutorial (from YT)
+#age = 35
+#price = 19.95
+#first_name = 'Jamie'
+#is_online = False
+#print(first_name,age,price)
+
+# Exercise 1
+#name = 'John Smith'
+#age = 20
+#new_patient = True
+#print(name, age, new_patient)
+
+# Receiving Input --> take the value from input function to store it as a variable
+#print("Hello " + name)
+
+# TYPE CONVERSION:
+    # numbers= 10, 20.8, etc.
+    # strings= "Jamie", "What did you eat today?", etc.
+    # Booleans= True or False
+
+# Convert the value of a variable from one type to another:
+    # int() = converts value into integer
+    # float() = converts value variable into decimal
+    # bool() = converts value into boolean
+    # str() = converts value into a string
+#birth_year = input("Enter your birth year: ")
+#age = 2023 - int(birth_year)
+#print(age)
+
+# Exercise 2:
+First: 14.5
+Second: 10
+Sum: 24.5
+
+first = input("First: ")
+    #Input functions always return a string variable!
+second = input("Second: ")
+sum = float(first) + float(second)
+print("Sum: " + str(sum))
+    #you have "Sum:" (a string), so it can only concatenate with another string variable
+
+first = float(input("First: "))
+second = float(input("Second: "))
+sum = first + second
+print("Sum: " + str(sum))
+
+# STRINGS:
+
+
+
+
+
